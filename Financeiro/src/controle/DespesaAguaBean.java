54d05ed42caeb0cfa75a3b2cdde11c4efbc59ad3
joaby <joabycavalcante@gmail.com>
2017-03-04 19:12:41 -0300

    atualizar e excluir agua

--- Financeiro/src/controle/DespesaAguaBean.java
+++ Financeiro/src/controle/DespesaAguaBean.java
@@ -15,8 +15,8 @@ import modelo.Mes;
 
 @ManagedBean
 @ViewScoped
-public class DespesaAguaBean extends AbstractBean implements Serializable{
-	
+public class DespesaAguaBean extends AbstractBean implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	private DespesaAgua despesaAgua;
 	private List<DespesaAgua> despesasAguas;
@@ -24,91 +24,108 @@ public class DespesaAguaBean extends AbstractBean implements Serializable{
 	private Mes mes;
 	private int ano;
 	private float despesaTotal;
-	
-	public DespesaAguaBean(){
+
+	public DespesaAguaBean() {
 		this.despesaAgua = new DespesaAgua();
 		this.despesasAguas = new ArrayList<DespesaAgua>();
 		this.meses = Arrays.asList(Mes.values());
 	}
-	
-	public void cadastrar(){
+
+	public void cadastrar() {
 		DespesaAguaDAO dDAO = new DespesaAguaJPADAO();
 		dDAO.save(this.despesaAgua);
 		displayInfoMessageToUser("Cadastrado com sucesso!");
 		this.despesaAgua = new DespesaAgua();
-		
+
+	}
+
+	public void selecionarParaAtualizar(DespesaAgua d) {
+		this.despesaAgua = d;
+		org.primefaces.context.RequestContext.getCurrentInstance().execute("PF('edit').show()");
 	}
 	
 	public void atualizar(){
-		
+		DespesaAguaDAO dDAO = new DespesaAguaJPADAO();
+		dDAO.save(this.despesaAgua);
+		displayInfoMessageToUser("Atualizado com sucesso!");
+		this.despesaAgua = new DespesaAgua();
 	}
-	
-	public void excluir(DespesaAgua a){
+
+	public void excluir(DespesaAgua a) {
 		DespesaAguaDAO dDAO = new DespesaAguaJPADAO();
 		dDAO.delete(a);
 		displayInfoMessageToUser("Excluido com sucesso!");
 		this.despesasAguas.remove(a);
 	}
-	
-	public void buscarPorAno(){
+
+	public void buscarPorAno() {
 		this.despesasAguas = new ArrayList<DespesaAgua>();
 		DespesaAguaDAO dDAO = new DespesaAguaJPADAO();
 		this.despesasAguas = dDAO.buscarPorAno(this.ano);
 		this.despesaTotal = somaDespesaTotal(this.despesasAguas);
 	}
-	
-	public void buscarPorMesAno(){
+
+	public void buscarPorMesAno() {
 		this.despesasAguas = new ArrayList<DespesaAgua>();
 		DespesaAguaDAO dDAO = new DespesaAguaJPADAO();
 		this.despesasAguas = dDAO.buscarPorMesAno(this.mes, this.ano);
 		this.despesaTotal = somaDespesaTotal(this.despesasAguas);
 	}
-	
-	public float somaDespesaTotal(List<DespesaAgua> d){
+
+	public float somaDespesaTotal(List<DespesaAgua> d) {
 		float total = 0;
-		for(int i = 0; i < d.size(); i++){
+		for (int i = 0; i < d.size(); i++) {
 			total += d.get(i).getValor();
 		}
 		return total;
 	}
-	
+
 	public DespesaAgua getDespesaAgua() {
 		return despesaAgua;
 	}
+
 	public void setDespesaAgua(DespesaAgua despesaAgua) {
 		this.despesaAgua = despesaAgua;
 	}
+
 	public List<DespesaAgua> getDespesasAguas() {
 		return despesasAguas;
 	}
+
 	public void setDespesasAguas(List<DespesaAgua> despesasAguas) {
 		this.despesasAguas = despesasAguas;
 	}
+
 	public List<Mes> getMeses() {
 		return meses;
 	}
+
 	public void setMeses(List<Mes> meses) {
 		this.meses = meses;
 	}
+
 	public Mes getMes() {
 		return mes;
 	}
+
 	public void setMes(Mes mes) {
 		this.mes = mes;
 	}
+
 	public int getAno() {
 		return ano;
 	}
+
 	public void setAno(int ano) {
 		this.ano = ano;
 	}
+
 	public float getDespesaTotal() {
 		return despesaTotal;
 	}
+
 	public void setDespesaTotal(float despesaTotal) {
 		this.despesaTotal = despesaTotal;
 	}
-	
-	
 
 }
