a57d42721141530bc25c2b373ad1448bd7cbe556
jackson freitas <jackson.freitas7@gmail.com>
2017-03-09 22:11:12 -0300

    Fiz o restante do usuário

--- Financeiro/src/controle/UsuarioBean.java
+++ Financeiro/src/controle/UsuarioBean.java
@@ -1,33 +1,42 @@
 package controle;
 
 import java.io.Serializable;
-
-import dao.AlunoDAO;
-import dao.AlunoJPADAO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import dao.UsuarioDAO;
 import dao.UsuarioJPADAO;
-import modelo.Aluno;
 import modelo.Usuario;
 
+@ManagedBean
+@ViewScoped
 public class UsuarioBean extends AbstractBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Usuario usuario;
-	
-	public UsuarioBean(){
-		
+	private List<Usuario> usuarios;
+
+	public UsuarioBean() {
+		this.usuario = new Usuario();
+		this.usuarios = new ArrayList<Usuario>();
 	}
-	
-	public void cadastrar(){
+
+	public void buscarTodos() {
+		this.usuarios = new ArrayList<Usuario>();
+		UsuarioDAO usuarioDAO = new UsuarioJPADAO();
+		this.usuarios = usuarioDAO.find();
+	}
+
+	public void cadastrar() {
 		try {
 			UsuarioDAO usuarioDAO = new UsuarioJPADAO();
 			Usuario u = usuarioDAO.find(this.usuario.getNome());
-			if(u == null){
+			if (u == null) {
 				usuarioDAO.save(this.usuario);
 				displayInfoMessageToUser("Cadastrado com sucesso!");
 				this.usuario = new Usuario();
-			}
-			else{
+			} else {
 				displayErrorMessageToUser("Usuário já cadastrado!");
 				this.usuario = new Usuario();
 			}
@@ -36,22 +45,52 @@ public class UsuarioBean extends AbstractBean implements Serializable {
 			this.usuario = new Usuario();
 		}
 	}
-	
-	public void atualizar(){
+
+	public void atualizar() {
 		UsuarioDAO usuarioDAO = new UsuarioJPADAO();
 		usuarioDAO.save(this.usuario);
 		displayInfoMessageToUser("Atualizado com sucesso!");
 		this.usuario = new Usuario();
 	}
-	
-	public void excluir(){
-		try {
-			UsuarioDAO usuarioDAO = new UsuarioJPADAO();
-			usuarioDAO.delete(usuario);
-			displayInfoMessageToUser("Excluido com sucesso!");
-			this.usuario = new Usuario();
-		} catch (Exception e) {
-			displayErrorMessageToUser("Erro ao excluir!");
-		}
+
+	public void excluir(Usuario u) {
+		UsuarioDAO usuarioDAO = new UsuarioJPADAO();
+		usuarioDAO.delete(u);
+		displayInfoMessageToUser("Excluido com sucesso!");
+		this.usuarios.remove(u);
 	}
+
+	public int sortByNome(String n1, String n2) {
+		return n1.compareTo(n2);
+	}
+	
+	public int sortByEmail(String e1, String e2) {
+		return e1.compareTo(e2);
+	}
+	
+	public int sortByLogin(String l1, String l2) {
+		return l1.compareTo(l2);
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
 }
--- Financeiro/src/modelo/Usuario.java
+++ Financeiro/src/modelo/Usuario.java
@@ -1,13 +1,39 @@
 package modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name="Usuario.buscarAtivo", 
+			query="SELECT u FROM Usuario u WHERE u.ativo = true"),
+	@NamedQuery(name="Usuario.buscarInativo", 
+			query="SELECT u FROM Usuario u WHERE u.ativo = false")	
+})
+
 public class Usuario {
 
+	@Id
 	public String login;
 	public String email;
 	public String nome;
 	public String senha;
+	private boolean ativo;
 	
+	public Usuario(){
+		this.ativo = true;
+	}
 	
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
 	public String getLogin() {
 		return login;
 	}
